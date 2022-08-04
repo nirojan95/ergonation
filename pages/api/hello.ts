25b8500b57b9cf44225fe4eea5af150ceed0f0fe
nitcher <nitcherkar@protonmail.com>
2022-08-05 00:32:15 +0100

    Fix: hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function Hello (_: NextApiRequest, res: NextApiResponse) => {
+export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' })
 }
 
